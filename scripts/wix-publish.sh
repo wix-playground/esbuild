56a57e6ba3f8bdc1e2f3b53ee577dacc6f365535
Sergey Ilyevsky <sergeyi@wix.com>
2023-06-09 16:06:19 +0300

    fixed wix-publish.sh

--- scripts/wix-publish.sh
+++ scripts/wix-publish.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 version=$1
 [ -n "$version" ] || {
@@ -11,9 +11,12 @@ version=$1
   exit 1
 }
 
-list="darwin-arm64 darwin-x64 linux-arm linux-arm64 linux-ia32 linux-loong64 linux-mips64el linux-ppc64 linux-riscv64 linux-s390x linux-x64"
+echo $version > version.txt
+make platform-all
 
-deps={\"$(echo ${list[@]/%/\":\"$version\"} | sed -e 's/ /","/g')}
+list=(darwin-arm64 darwin-x64 linux-arm linux-arm64 linux-ia32 linux-loong64 linux-mips64el linux-ppc64 linux-riscv64 linux-s390x linux-x64)
+
+deps={\"@esbuild/$(echo ${list[@]/%/\":\"$version\"} | sed -e 's| |,"@esbuild/|g')}
 jq ". += {\"dependencies\":${deps}}" npm/esbuild/package.json > npm/esbuild/package.json_
 mv npm/esbuild/package.json{_,}
 
@@ -21,7 +24,7 @@ git commit -am "version $version"
 git tag $version
 git push -u origin $version
 
-for b in $list; do
+for b in ${list[@]}; do
   pushd npm/\@esbuild/$b
     npm publish
   popd
