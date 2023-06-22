b189671e07e1d9265df8cf06524b7c6488f62479
Sergey Ilyevsky <sergeyi@wix.com>
2023-06-22 16:48:47 +0300

    Fixed publishing

--- scripts/wix-publish.sh
+++ scripts/wix-publish.sh
@@ -14,15 +14,7 @@ version=$1
 echo $version > version.txt
 make platform-all
 
-list=(darwin-arm64 darwin-x64 linux-arm linux-arm64 linux-ia32 linux-loong64 linux-mips64el linux-ppc64 linux-riscv64 linux-s390x linux-x64)
-
-deps={\"@esbuild/$(echo ${list[@]/%/\":\"$version\"} | sed -e 's| |,"@esbuild/|g')}
-jq ". += {\"dependencies\":${deps}}" npm/esbuild/package.json > npm/esbuild/package.json_
-mv npm/esbuild/package.json{_,}
-
-git commit -am "version $version"
-git tag $version
-git push -u origin $version
+list=(`jq .optionalDependencies npm/esbuild/package.json | grep esbuild | cut -d \" -f2 | cut -d/ -f2`)
 
 for b in ${list[@]}; do
   pushd npm/\@esbuild/$b
@@ -32,3 +24,7 @@ done
 pushd npm/esbuild
   npm publish
 popd
+
+git commit -am "version $version"
+git tag $version
+git push -u origin $version
